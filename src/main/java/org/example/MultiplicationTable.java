750500c669eebe06e7296f93e0ea10c6115905ad
Ekaterina Sushkova <sushkovakeit@yandex.ru>
2023-08-29 17:53:24 +0300

    add MultiplicationTable

---
+++ src/main/java/org/example/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for(int i = 1; i<=10; i++){
+            for(int b = 1; b<=10; b++) {
+                System.out.printf("%d*%d=%d ", b, i, b*i);
+            }
+            System.out.println(" ");
+            for (int c = 1; c <= 10; c++) {
+                System.out.print("--------");
+            }
+            System.out.println();
+        }
+    }
+}
